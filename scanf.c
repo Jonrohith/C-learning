e86bf3483a8c1c79d0d8aa0e83e8dab49110b7f3
Jonrohith <uthira90.uk@gmail.com>
2026-07-06 20:58:52 +0530

    test

--- scanf.c
+++ scanf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
   int fav_num;
-printf("Enter your favourite number here");
+printf("Enter your favourite number here:");
 scanf("%d",&fav_num);
-printf("Your favourite number is %d",fav_num);
+printf("Your favourite number is %d.",fav_num);
 return 0;
 }
 
