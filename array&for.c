73c111ca1b9125f54c312114f4fb1a5535cad2bb
Jonrohith <uthira90.uk@gmail.com>
2026-07-06 21:09:31 +0530

    syntac errors solved

--- array&for.c
+++ array&for.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
 int marks[]={45,34,67,89,100};
-for (i=0;i<4;i=++) {
-printf("The person %d secured %d marks",i,marks[i]);
-return 0;} 
+for (int i=0;i<5;i++) {
+printf("The person %d secured %d marks\n",i,marks[i]);
+}
+return 0;
 }
